f2507b642c62af71e06ce2685f38e48049b84298
Chicshin <inchicshin@gmail.com>
2019-08-14 11:58:07 +0900

    added timestamp functions

--- Model/ChatModel.swift
+++ Model/ChatModel.swift
@@ -24,6 +24,37 @@ class ChatModel: NSObject{
         return senderUid == Auth.auth().currentUser?.uid ? toUid : senderUid
     }
     
+    func timestampString() -> String? {
+        var timeString: String?
+        if let seconds = timestamp?.doubleValue {
+            let timestampDate = NSDate(timeIntervalSince1970: seconds)
+            print(timestampDate)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "hh:mm a"
+            timeString = dateFormatter.string(from: timestampDate as Date)
+        }
+        return timeString
+    }
+    
+    func datestampString() -> String? {
+        var dateString: String?
+        if let seconds = timestamp?.doubleValue {
+            let timestampDate = NSDate(timeIntervalSince1970: seconds)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            dateString = dateFormatter.string(from: timestampDate as Date)
+        }
+        return dateString
+    }
+    func today() -> String? {
+        var today: String?
+        let timestamp: NSNumber = NSNumber(value: Int(NSDate().timeIntervalSince1970))
+        let timestampDate = NSDate(timeIntervalSince1970: TimeInterval(truncating: timestamp))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        today = dateFormatter.string(from: timestampDate as Date)
+        return today
+    }
     init(dictionary: [String: Any]) {
         super.init()
         senderUid = dictionary["senderUid"] as? String
--- Model/toremove.swift
+++ Model/toremove.swift
@@ -31,22 +31,3 @@ import Foundation
 //        self.view.endEditing(true)
 //    }
 
-
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        var cellToReturn = UITableViewCell()
-
-//            cell.timestamp(chat: chat)
-
-
-
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-//        addSubview(timestamp)
-
-
-//        constraints()
-
-//        timestamp.font = UIFont.systemFont(ofSize: 12)
-//        timestamp.textColor = .lightGray
